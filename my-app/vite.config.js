46b2e3403aa799f68fbb364d0b78b90f6e9523f1
lydiaBn <94543559+lydiaBn@users.noreply.github.com>
2025-11-04 16:58:33 +0100

    Ensure Supabase installed and build fixed

--- my-app/vite.config.js
+++ my-app/vite.config.js
@@ -1,12 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   optimizeDeps: {
     include: ["@supabase/supabase-js"],
   },
+  build: {
+    rollupOptions: {
+      external: [],
+    },
+  },
   ssr: {
     noExternal: ["@supabase/supabase-js"],
   },
-})
+});
